d514cecce96779dde29810330c20674b654cc055
fnunez <francisconunez@outlook.com>
2020-03-01 20:25:45 +0100

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
@@ -33,12 +28,14 @@ namespace API
             });
 
             services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
-
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-
                 });
             });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+
             services.AddControllers();
         }
 
@@ -53,7 +50,6 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseCors("CorsPolicy");
